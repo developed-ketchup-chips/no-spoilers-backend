7b91f320e4df46b882cb36c4a7fc362366516ff8
Mihiri Kamiss <76460057+Jaymerist@users.noreply.github.com>
2023-09-16 19:05:46 -0600

    Created sample media collection data

--- model/pqr.py
+++ model/pqr.py
@@ -1,15 +1,67 @@
-import pymongo 
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://mihiri:mihiri@cluster0.ey8zati.mongodb.net/")
 
-client = pymongo.MongoClient("mongodb+srv://saphia:saphia@cluster0.ey8zati.mongodb.net/")
+# Access the "mydb" database (it will be created if it doesn't exist)
+db = client["mydb"]
 
-information = mydb.table1
+# Insert data into a collection within the "mydb" database
+media_collection = db["media"]
 
-rec[{
-    "EMPNO": 7934,
-    "ENAME": "MILLER",
-    "JOB": "CLERK",
-    "MGR": 7782,
-    "SAL": 1300,
-    "DEPTNO": 10
-}]
-information.insert_many(rec)
+#Sample media content
+#add image files to media used for presentation
+
+media1 = {
+    "_id" : 1,
+    "title" : "Anne With an E",
+    "episode_count" : 27
+}
+
+media2 = {
+    "_id" : 2,
+    "title" : "Attack on Titan",
+    "episode_count" : 88
+}
+
+media3 = {
+    "_id" : 3,
+    "title" : "1984",
+    "episode_count" : 24
+}
+
+media4 = {
+    "_id" : 4,
+    "title" : "Harry Potter",
+    "episode_count" : 25
+}
+
+media5 = {
+    "_id" : 5,
+    "title" : "To Kill a Mockingbird",
+    "episode_count" : 31
+}
+
+media6 = {
+    "_id" : 6,
+    "title" : "Breaking Bad",
+    "episode_count" : 125
+}
+
+media7 = {
+    "_id" : 7,
+    "title" : "Money Heist",
+    "episode_count" : 41
+}
+
+media8 = {
+    "_id" : 8,
+    "title" : "The Hobbit",
+    "episode_count" : 19 
+}
+
+media9 = {
+    "_id" : 8,
+    "title" : "One Piece",
+    "episode_count" : 1075 
+}
+
+
