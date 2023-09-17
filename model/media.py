244ca62451a2eaccbca5f4f2abdc900210346d91
Mihiri Kamiss <76460057+Jaymerist@users.noreply.github.com>
2023-09-16 18:29:53 -0600

    Updated room comments

--- model/media.py
+++ model/media.py
@@ -3,19 +3,29 @@
 from media_type import MediaType
 from user import User
 
-class Episode():
-    def __init__(self, name, commentList):
-        self.name = name,
-        self.status = False, #status is automatically false(incomplete)
-        self.commentList = commentList
-
 class Comment():
     def __init__(self, user, comment):
         if not comment.strip():  # Check if comment is blank or contains only whitespace
             raise ValueError("Comment cannot be blank.")
         self.user = user
         self.comment = comment
+        
 
+class CommentList():
+    def __init__(self):
+        self.comment_list = []
+
+    def add_comment(self, comment:Comment):
+        self.comment_list.append()
+
+    def remove_comment(self, comment:Comment):
+        self.comment_list.remove()
+
+class Episode():
+    def __init__(self, name, commentList):
+        self.name = name,
+        self.status = False, #status is automatically false(incomplete)
+        self.commentList = commentList
 
 class Media():
     def __init__(self, name, type:MediaType, length, episodeList):
